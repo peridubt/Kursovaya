e1c633dd349811c1b698a64a2db9a0bebb727d62
peridubt <43499978+peridubt@users.noreply.github.com>
2023-06-04 00:55:56 +0300

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -10,15 +10,11 @@ void add_info(Phonebook* a)
 }
 Array::Array(size_t arr_size)
 {   
-    size = arr_size;
-    if (size)
-    {
-        arr = new Phonebook * [size];
-        for (int i = 0; i < size; ++i)
-            arr[i] = new Phonebook;
-    }
-    else
-        arr = nullptr;
+    real_size = arr_size;
+    fact_size = 0;
+    arr = new Phonebook * [real_size];
+    for (int i = 0; i < real_size; ++i)
+        arr[i] = new Phonebook;
 }
 //Array::~Array()
 //{
@@ -28,18 +24,18 @@ Array::Array(size_t arr_size)
 //}
 void Array::fill_array(std::ifstream& file)
 {
-    file.seekg(0);
-    file.read((char*)&size, sizeof(size));
-    if (size)
+    file.read((char*)&fact_size, sizeof(fact_size));
+    file.read((char*)&real_size, sizeof(real_size));
+    if (fact_size)
     {
-        arr = new Phonebook * [size];
-        for (int i = 0; i < size; ++i)
+        arr = new Phonebook * [real_size];
+        for (size_t i = 0; i < real_size; ++i)
             arr[i] = new Phonebook;
     }
     else
         arr = nullptr;
-    int i = 0;
-    while (i < size)
+    size_t i = 0;
+    while (i < fact_size)
     {
         file.read((char*)arr[i], sizeof(Phonebook));
         ++i;
@@ -48,49 +44,58 @@ void Array::fill_array(std::ifstream& file)
 void Array::clear_tmp()
 {
     arr = nullptr;
-    size = 0;
+    real_size = 0;
+    fact_size = 20;
 }
 void Array::print()
 {
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < fact_size; ++i)
         arr[i]->print(std::cout);
 }
 void Array::sorting()
 {
-    for (size_t count = size; count > 1; --count)
+    for (size_t count = fact_size; count > 1; --count)
         for (size_t i = 0; i < count - 1; ++i)
             if (arr[i]->compare(*arr[i + 1]) > 0)
                 std::swap(arr[i], arr[i + 1]);
 }
-void Array::arr_copy(ptrPhnbk* mas, int size_arr)
+void Array::arr_copy(ptrPhnbk* mas, size_t size_arr)
 {
-    for (int i = 0; i < size_arr; ++i)
+    for (size_t i = 0; i < size_arr; ++i)
         *arr[i] = *mas[i];
 }
 void Array::add_note()
 {
-    Array tmp(size + 1);
-    tmp.arr_copy(arr, size);
-    std::cout << "Введите данные о новом абоненте.\n\n";
-    add_info(tmp.get_arr()[size]);
-    for (size_t i = 0; i < size; ++i)
-        delete arr[i];
-    delete[] arr;
-    arr = tmp.get_arr();
-    ++size;
-    tmp.clear_tmp();
-    std::cin.ignore(std::cin.rdbuf()->in_avail());
-    std::cout << "\nЗапись добавлена!\n\n";
+    if (real_size != fact_size)
+    {
+        add_info(get_arr()[fact_size++]);
+    }
+    else
+    {
+        Array tmp(real_size + 20);
+        tmp.arr_copy(arr, fact_size);
+        std::cout << "Введите данные о новом абоненте.\n\n";
+        add_info(tmp.get_arr()[real_size]);
+        for (size_t i = 0; i < real_size; ++i)
+            delete arr[i];
+        delete[] arr;
+        arr = tmp.get_arr();
+        tmp.clear_tmp();
+        fact_size = real_size + 1;
+        real_size += 20;
+        std::cin.ignore(std::cin.rdbuf()->in_avail());
+        std::cout << "\nЗапись добавлена!\n\n";
+    }
 }
 void Array::edit_note()
 {
-    if (size != 0)
+    if (fact_size != 0)
     {
-        std::cout << "Введите номер записи от 1 до " << size << ", по которой будут изменяться данные:\n->\t";
+        std::cout << "Введите номер записи от 1 до " << fact_size << ", по которой будут изменяться данные:\n->\t";
         long int note;
         std::cin >> note;
         
-        while (note < 1 || note > size)
+        while (note < 1 || note > fact_size)
         {
             std::cout << "Введённое значение не находится в указанном промежутке!\nПовторите попытку ввода:\n->\t";
             std::cin >> note;
@@ -106,13 +111,12 @@ void Array::edit_note()
 }
 void Array::delete_note()
 {
-    if (size != 0)
+    if (fact_size != 0)
     {
-        Array tmp(size - 1);
-        std::cout << "Введите номер записи от 1 до " << size << ", по которому она будет удалена:\n->\t";
+        std::cout << "Введите номер записи от 1 до " << fact_size << ", по которому она будет удалена:\n->\t";
         long int note;
         std::cin >> note;
-        while (note < 1 || note > size)
+        while (note < 1 || note > fact_size)
         {
             std::cout << "Введённое значение не находится в указанном промежутке!\nПовторите попытку ввода:\n->\t";
             std::cin.clear();
@@ -120,21 +124,16 @@ void Array::delete_note()
             std::cin >> note;
         }
         size_t i = 0, new_i = 0;
-        while (i < size)
+        while (i < fact_size)
         {
             if (i != note - 1)
             {
-                *tmp.get_arr()[new_i] = *arr[i];
+                arr[new_i] = arr[i];
                 ++new_i;
             }
             ++i;
         }
-        for (size_t i = 0; i < size; ++i)
-            delete arr[i];
-        delete[] arr;
-        arr = tmp.get_arr();
-        --size;
-        tmp.clear_tmp();
+        --fact_size;
         std::cout << "Запись №" << note << " удалена!\n\n";
     }
     else
@@ -145,10 +144,11 @@ bool Array::to_binary(const char* file_name) // Проверяет, открыл
 {
     bool result = true;
     std::ofstream file(file_name, std::ios::binary | std::ios::out);
-    file.write((char*)&size, sizeof(size));
+    file.write((char*)&fact_size, sizeof(fact_size));
+    file.write((char*)&real_size, sizeof(real_size));
     if (file)
     {
-        for (size_t i = 0; i < size; ++i)
+        for (size_t i = 0; i < fact_size; ++i)
             file.write((char*)arr[i], sizeof(Phonebook));
         file.close();
     }
@@ -159,14 +159,15 @@ bool Array::to_binary(const char* file_name) // Проверяет, открыл
 // перед заданием тоже спрашивать про изменения в файле
 void Array::task(std::istream& file_in, std::ostream& file_out, int num_search, char* file_out_name)
 {
-    int size = 0, i = 0;
+    size_t f_size = 0, r_size = 0, i = 0;
     Phonebook note;
     bool find = false;
-    file_in.read((char*)&size, sizeof(size));
-    if (size)
+    file_in.read((char*)&f_size, sizeof(f_size));
+    file_in.read((char*)&r_size, sizeof(r_size));
+    if (f_size)
     {
         file_out << "Здесь представлены данные абонентов, у которых номер телефона начинается на \"" << num_search << "\".\n\n";
-        while (i < size)
+        while (i < f_size)
         {
             ++i;
             file_in.read((char*)&note, sizeof(note));
@@ -180,7 +181,7 @@ void Array::task(std::istream& file_in, std::ostream& file_out, int num_search, 
         if (!find)
         {
             file_out << "Абонентов, чьи номера телефонов начинаются на \"" << num_search << "\", нет";
-            std::cout << "Абонентов, чьи номера телефонов начинаются на \"" << num_search << "\", нет";
+            std::cout << "Абонентов, чьи номера телефонов начинаются на \"" << num_search << "\", нет\n";
             std::cout << "Данные внесены в файл \"" << file_out_name << "\"!\n\n";
         }
         else
@@ -193,12 +194,12 @@ void Array::text_output(std::istream& file_in, std::ostream& stream)
 {
     int i = 0;
     Phonebook note;
-    file_in.seekg(0);
-    file_in.read((char*)&size, sizeof(size));
-    if (size!=0)
+    file_in.read((char*)&fact_size, sizeof(fact_size));
+    file_in.read((char*)&real_size, sizeof(real_size));
+    if (fact_size!=0)
     {
-        stream << "\nКоличество записей:\t" << size << "\n\n";
-        while (i < size)
+        stream << "\nКоличество записей:\t" << fact_size << "\n\n";
+        while (i < fact_size)
         {
             ++i;
             file_in.read((char*)&note, sizeof(note));
